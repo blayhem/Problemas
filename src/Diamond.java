19464ecdbe266337189bd51207e787fae9ad184d
Daniel Fernandez Rico <deferrico@gmail.com>
2015-07-10 20:19:13 +0200

    Lucy in the sky with diamonds (Codewars)
    You need to print a shape on the screen using asterisk ("*") characters.
    
    The shape that will be returned from print method resembles a diamond,
    where the number provided as input represents the number of *’s printed
    on the middle line. The line above and below will be centered and will
    have 2 less *’s than the middle line. This reduction by 2 *’s for each
    line continues until a line with a single * is printed at the top and
    bottom of the figure.
    
    Return null if input is even number (as it is not possible to print
    diamond with even number).

---
+++ src/Diamond.java
@@ -0,0 +1,37 @@
+/**
+ * Created by blayhem on 10/07/15.
+ */
+public class Diamond {
+    public static String print(int n) {
+        if(n % 2 == 0 || n < 0) return null;   //only even numbers are valid
+        String diamond = "";
+        for (int i = 1; i <= (n /2) + 1; i++) {
+            for (int j = 0; j < (n / 2) - i + 1; j++)
+                System.out.printf(" ");
+                diamond+=" ";
+            for (int j = 0; j < (i*2) - 1; j++)
+                System.out.printf("*");
+                diamond+="*";
+            for (int j = 0; j < (n / 2) - i + 1; j++) System.out.printf(" ");
+            System.out.printf("\n");
+            diamond+="\n";
+        }
+        for (int i = n / 2; i > 0; i--) {
+            for (int j = 0; j < (n / 2) - i + 1; j++)
+                System.out.printf(" ");
+                diamond+=" ";
+            for (int j = 0; j < (i*2) - 1; j++)
+                System.out.printf("*");
+                diamond+="*";
+            for (int j = 0; j < (n / 2) - i + 1; j++) System.out.printf(" ");
+            System.out.printf("\n");
+            diamond+="\n";
+        }
+        return diamond;
+    }
+
+    public static void main(String[] args) {
+        print(7);
+    }
+
+}
